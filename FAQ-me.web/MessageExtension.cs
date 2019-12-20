a09847f503fee507ce6fdbef239a41e23081f394
Nick Kramer (Teams) <nkramer@microsoft.com>
2019-12-20 11:00:24 -0800

    stock answers

--- FAQ-me.web/MessageExtension.cs
+++ FAQ-me.web/MessageExtension.cs
@@ -25,25 +25,51 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web
                 title = titleParam.Value.ToString();
             }
 
-            var attachments = new ComposeExtensionAttachment[5];
-            for (int i = 0; i < 5; i++)
+            var attachments = new ComposeExtensionAttachment[]
             {
-                attachments[i] = GetAttachment(title);
-            }
+                GetAttachment("which API",
+                @"In order to give you a precise answer, could you clarify:
+- The URL and http verb for the method of interest (eg: POST https://graph.microsoft.com/beta/teams/{id}/channels)
+- Application permissions or user delegated permissions?
+
+Thx.
+"),
+                                GetAttachment("API failed",
+                @"In order to give you a precise answer, could you clarify:
+The URL and http verb for the method of interest(eg: POST https://graph.microsoft.com/beta/teams/{id}/channels)
+Application permissions or user delegated permissions ?
+The request - id from a call that's failed within the last 24 hours (we only keep a few days of logs)
+The payload you passed in, and the payload you got back
+The http response code
+If it was a 401 or 403, why do you think this was a mistake? What permission scopes are you calling it with?
+
+Thx.
+"),
+                                GetAttachment("Roadmap",
+                @"Our roadmap is available at _________________, please let me know if you have any questions."),
+                                GetAttachment("Not planned", "We do not currently have plans to build that feature."),
+        };
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    attachments[i] = GetAttachment(title);
+            //}
 
             var response = new ComposeExtensionResponse(new ComposeExtensionResult("list", "result"));
+            //var response = new ComposeExtensionResponse(new ComposeExtensionResult("list", type: "message", text: "foo"));
             response.ComposeExtension.Attachments = attachments.ToList();
+            //response.ComposeExtension.Text = "faq me!";
 
             return response;
         }
 
-        private static ComposeExtensionAttachment GetAttachment(string title = null)
+        private static ComposeExtensionAttachment GetAttachment(string title, string body)
         {
             var card = new ThumbnailCard
             {
                 Title = !string.IsNullOrWhiteSpace(title) ? title : Faker.Lorem.Sentence(),
-                Text = Faker.Lorem.Paragraph(),
-               Images = new System.Collections.Generic.List<CardImage> { new CardImage("http://lorempixel.com/640/480?rand=" + DateTime.Now.Ticks.ToString()) }
+                Text = body,
+               //Images = new System.Collections.Generic.List<CardImage> { new CardImage("http://lorempixel.com/640/480?rand=" + DateTime.Now.Ticks.ToString()) }
             };
             
             return card
